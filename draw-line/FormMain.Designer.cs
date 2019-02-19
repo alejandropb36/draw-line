a7f03e85302e158555c1d7cfe506396023714007
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-02-18 23:53:16 -0600

    Cambio a PictureBox y tratando de corregir bresenham

--- draw-line/FormMain.Designer.cs
+++ draw-line/FormMain.Designer.cs
@@ -28,21 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.workSpace1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.buttonClear = new System.Windows.Forms.Button();
+            this.workSpace = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.workSpace)).BeginInit();
             this.SuspendLayout();
-            // 
-            // workSpace1
-            // 
-            this.workSpace1.BackColor = System.Drawing.SystemColors.Window;
-            this.workSpace1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.workSpace1.Location = new System.Drawing.Point(12, 133);
-            this.workSpace1.Name = "workSpace1";
-            this.workSpace1.Size = new System.Drawing.Size(500, 500);
-            this.workSpace1.TabIndex = 0;
-            this.workSpace1.Paint += new System.Windows.Forms.PaintEventHandler(this.workSpace1_Paint);
-            this.workSpace1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.workSpace1_MouseClick);
             // 
             // label1
             // 
@@ -65,26 +55,36 @@
             this.buttonClear.UseVisualStyleBackColor = true;
             this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
             // 
+            // workSpace
+            // 
+            this.workSpace.BackColor = System.Drawing.SystemColors.Window;
+            this.workSpace.Location = new System.Drawing.Point(22, 123);
+            this.workSpace.Name = "workSpace";
+            this.workSpace.Size = new System.Drawing.Size(500, 500);
+            this.workSpace.TabIndex = 5;
+            this.workSpace.TabStop = false;
+            this.workSpace.MouseClick += new System.Windows.Forms.MouseEventHandler(this.workSpace_MouseClick);
+            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(534, 645);
+            this.Controls.Add(this.workSpace);
             this.Controls.Add(this.buttonClear);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.workSpace1);
             this.Name = "FormMain";
             this.Text = "Prinpal";
+            ((System.ComponentModel.ISupportInitialize)(this.workSpace)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel workSpace1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.PictureBox workSpace;
     }
 }
 
--- draw-line/FormMain.cs
+++ draw-line/FormMain.cs
@@ -12,104 +12,27 @@ namespace draw_line
 {
     public partial class FormMain : Form
     {
-        Line line;
-        int initialX;
-        int initialY;
-        int finalX;
-        int finalY;
+        Point initial, final;
+        Bitmap bmp;
 
         public FormMain()
         {
-            line = new Line();
+            initial = new Point(-1, -1);
+            final = new Point(-1, -1);
+            bmp = new Bitmap(500, 500);
             InitializeComponent();
-
-            initialX = initialY = -1;
-            finalX = finalY = -1;
         }
-
-        private void drawGrid(Panel panel)
-        {
-            int x, y, height, width, linesWidth, linesHeight;
-            Pen pen = new Pen(Color.LightGray, 1);
-            const int sizeSquare = 20;
-
-            height = panel.Height;
-            width = panel.Width;
-            linesWidth = width / sizeSquare;
-            linesHeight = height / sizeSquare;
-
-            y = 0;
-            for (int i = 0; i < linesWidth; i++)
-            {
-                x = sizeSquare * i;
-                panel.CreateGraphics().DrawLine(pen, x, y, x, height);
-            }
-
-            x = 0;
-            for (int i = 0; i < linesHeight; i++)
-            {
-                y = sizeSquare * i;
-                panel.CreateGraphics().DrawLine(pen, x, y, width, y);
-            }
-        }
-
-        private void workSpace1_Paint(object sender, PaintEventArgs e)
-        {
-            drawGrid(workSpace1);
-        }
-
-        /*
-        private void workSpace2_Paint(object sender, PaintEventArgs e)
-        {
-        }
-        */
 
         private void buttonClear_Click(object sender, EventArgs e)
         {
-            workSpace1.Refresh();
-            initialX = initialY = -1;
-            finalX = finalY = -1;
-            line.getInitialPoint().setX(initialX);
-            line.getInitialPoint().setY(initialY);
-            line.getFinalPoint().setX(finalX);
-            line.getFinalPoint().setY(finalY);
+            initial.X = initial.Y = -1;
+            final.X = final.Y = -1;
+
+            bmp = new Bitmap(500, 500);
+            workSpace.Image = bmp;
         }
 
-        private void workSpace1_MouseClick(object sender, MouseEventArgs e)
-        {
-            Point initialPoint;
-            Point finalPoint;
-            Pen pen = new Pen(Color.Orange, 3);
-
-            if (initialX == -1)
-            {
-                initialX = e.X;
-                initialY = e.Y;
-                initialPoint = new Point(initialX, initialY);
-                line.setInitialPoint(initialPoint);
-                workSpace1.CreateGraphics().DrawEllipse(pen, initialX, initialY, 3, 3);
-            }
-            else
-            {
-                finalX = e.X;
-                finalY = e.Y;
-                finalPoint = new Point(finalX, finalY);
-                line.setFinalPoint(finalPoint);
-                workSpace1.CreateGraphics().DrawEllipse(pen, finalX, finalY, 3, 3);
-
-                Console.WriteLine("------------- Puntos ------------------");
-                Console.WriteLine("punto inicial: (" + initialX + ", " + initialY + ")");
-                Console.WriteLine("punto final: (" + finalX + ", " + finalY + ")");
-
-                DDA(workSpace1, line);
-                bresenham(workSpace1, line);
-                initialX = initialY = -1;
-                finalX = finalY = -1;
-            }
-            
-        }
-
-        private void DDA(Panel workSpace, Line line)
+        private void DDA(PictureBox workSpace, Point initial, Point final)
         {
             double xi, yi;
             double xf, yf;
@@ -117,25 +40,22 @@ namespace draw_line
             double m, b;
             int incremento;
             int xact, yact;
-            Pen pen;
-
-            pen = new Pen(Color.Red, 1);
-
+            
             incremento = 1;
 
-            xi = (double)(line.getInitialPoint().getX());
-            yi = (double)(line.getInitialPoint().getY());
-            xf = (double)(line.getFinalPoint().getX());
-            yf = (double)(line.getFinalPoint().getY());
+            xi = (double)initial.X;
+            yi = (double)initial.Y;
+            xf = (double)final.X;
+            yf = (double)final.Y;
 
             deltaX = xf - xi;
             deltaY = yf - yi;
 
-            if(Math.Abs(deltaX) > Math.Abs(deltaY))
+            if (Math.Abs(deltaX) > Math.Abs(deltaY))
             {
                 m = deltaY / deltaX;
                 b = yi - (m * xi);
-                if(deltaX < 0)
+                if (deltaX < 0)
                 {
                     incremento = -1;
                 }
@@ -144,20 +64,20 @@ namespace draw_line
                     incremento = 1;
                 }
 
-                for(int i = (int)xi; i != xf; i += incremento)
+                for (int i = (int)xi; i != xf; i += incremento)
                 {
                     yact = (int)(Math.Round((m * i) + b));
-                    workSpace.CreateGraphics().DrawEllipse(pen, i, yact, 1, 1);
+                    bmp.SetPixel(i, yact, Color.Blue);
                 }
             }
             else
             {
-                if(deltaY != 0)
+                if (deltaY != 0)
                 {
                     m = deltaX / deltaY;
                     b = xi - (m * yi);
 
-                    if(deltaY < 0)
+                    if (deltaY < 0)
                     {
                         incremento = -1;
                     }
@@ -166,22 +86,25 @@ namespace draw_line
                         incremento = 1;
                     }
 
-                    for(int i = (int)yi; i != yf; i += incremento)
+                    for (int i = (int)yi; i != yf; i += incremento)
                     {
                         xact = (int)(Math.Round((m * i) + b));
-                        workSpace.CreateGraphics().DrawEllipse(pen, xact, i, 1, 1);
+                        bmp.SetPixel(xact, i, Color.Blue);
                     }
                 }
             }
+
+            workSpace.Image = bmp;
         }
 
-        private void bresenham(Panel workSpace, Line line)
+        /*
+
+        private void bresenham(PictureBox workSpace, Point initial, Point final)
         {
-            Pen pen = new Pen(Color.Green, 1);
-            double xi = (double)line.getInitialPoint().getX();
-            double yi = (double)line.getInitialPoint().getY();
-            double xf = (double)line.getFinalPoint().getX();
-            double yf = (double)line.getFinalPoint().getY();
+            double xi = (double)initial.X;
+            double yi = (double)initial.Y;
+            double xf = (double)final.X;
+            double yf = (double)final.Y;
             double dx, dx2;
             double dy, dy2;
             double x_k, y_k, p_k;
@@ -209,19 +132,19 @@ namespace draw_line
             {
                 stepx = 1;
             }
-            
+
             x_k = xi;
             y_k = yi;
             dy2 = (dy * 2.0);
             dx2 = (dx * 2.0);
-            
-            if(dx > dy)
+
+            if (dx > dy)
             {
                 p_k = dy2 - dx;
-                while(x_k != xf)
+                while (x_k != xf)
                 {
                     x_k += stepx;
-                    if(p_k <= 0)
+                    if (p_k <= 0)
                     {
                         p_k += dy2;
                     }
@@ -230,7 +153,7 @@ namespace draw_line
                         y_k += stepy;
                         p_k += dy2 - dx2;
                     }
-                    workSpace.CreateGraphics().DrawEllipse(pen, (int)x_k, (int)y_k, 1, 1);
+                    bmp.SetPixel((int)x_k, (int)y_k, Color.Orange);
                 }
             }
             else
@@ -248,12 +171,119 @@ namespace draw_line
                         x_k += stepx;
                         p_k = dx2 - dy2;
                     }
-                    workSpace.CreateGraphics().DrawEllipse(pen, (int)x_k, (int)y_k, 1, 1);
+                    bmp.SetPixel((int)x_k, (int)y_k, Color.Orange);
                 }
             }
+            workSpace.Image = bmp;
 
         }
+        */
+        
+        private void bresenham(PictureBox workSpace, Point initial, Point final)
+        {
+            double xi = (double)initial.X;
+            double yi = (double)initial.Y;
+            double xf = (double)final.X;
+            double yf = (double)final.Y;
+            double dx = xf - xi;
+            double dy = yf - yi;
+            double dx2 = dx * 2.0;
+            double dy2 = dy * 2.0;
+            double m = dy / dx;
+            double b = yi - (m * xi);
+            double c = -(dx2 * b) + dx - dy2;
+            double x_k, y_k, p_k;
+            int stepy = 1, stepx = 1; ;
 
+            x_k = xi;
+            y_k = yi;
+
+            p_k = (2.0 * yi) * dx - (dy * 2.0) * xi + c;
+
+            if(dx > dy)
+            {
+                if(yi > yf)
+                {
+                    stepy = -1;
+                }
+                if(xi > xf)
+                {
+                    stepx = -1;
+                }
+
+                while (x_k < xf)
+                {
+                    if (p_k >= 0)
+                    {
+                        p_k += -Math.Abs(dy2);
+                        bmp.SetPixel((int)x_k, (int)y_k, Color.Orange);
+                    }
+                    else
+                    {
+                        y_k += stepy;
+                        p_k += -Math.Abs(dy2) + Math.Abs(dx2);
+                        bmp.SetPixel((int)x_k, (int)y_k, Color.Orange);
+                    }
+                    x_k += stepx;
+                }
+            }
+            else
+            {
+                if (yi > yf)
+                {
+                    stepy = -1;
+                }
+                if (xi > xf)
+                {
+                    stepx = -1;
+                }
+
+                while (y_k < yf)
+                {
+                    if (p_k >= 0)
+                    {
+                        p_k += - Math.Abs(dy2);
+                        bmp.SetPixel((int)x_k, (int)y_k, Color.Orange);
+                    }
+                    else
+                    {
+                        x_k += stepx;
+                        p_k += - Math.Abs(dy2) + Math.Abs(dx2);
+                        bmp.SetPixel((int)x_k, (int)y_k, Color.Orange);
+                    }
+                    y_k += stepy;
+                }
+            }
+            workSpace.Image = bmp;
+        }
+
+        private void workSpace_MouseClick(object sender, MouseEventArgs e)
+        {
+            Pen pen = new Pen(Color.Orange, 3);
+
+            if (initial.X == -1)
+            {
+                initial.X = e.X;
+                initial.Y = e.Y;
+                workSpace.CreateGraphics().DrawEllipse(pen, initial.X, initial.Y, 3, 3);
+            }
+            else
+            {
+                final.X = e.X;
+                final.Y = e.Y;
+                workSpace.CreateGraphics().DrawEllipse(pen, final.X, final.Y, 3, 3);
+
+                Console.WriteLine("------------- Puntos ------------------");
+                Console.WriteLine("punto inicial: (" + initial.X + ", " + initial.Y + ")");
+                Console.WriteLine("punto final: (" + final.X + ", " + final.Y + ")");
+
+                DDA(workSpace, initial, final);
+                bresenham(workSpace, initial, final);
+                initial.X = initial.Y = -1;
+                final.X = final.Y = -1;
+            }
+        }
+        
         
     }
 }
